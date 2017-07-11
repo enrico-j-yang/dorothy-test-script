a09c2df02499cbe6f4d9914379d5b247b79058b0
carlton <carlton@texchi.com>
2017-07-11 15:05:18 +0800

    燃油报警修改

--- features/steps/surplus_fuel.py
+++ features/steps/surplus_fuel.py
@@ -11,7 +11,7 @@ from behave import *
 
 @when(u'CANBUS上有剩余油量为{surplus_fuel}升')
 def step_impl(context, surplus_fuel):
-    context.dorothyTestInput.sysExtEvt.set_value('SurplusFuel', int(surplus_fuel))
+    context.dorothyTestInput.sysExtEvt.set_value('SurplusFuel', float(surplus_fuel))
 
 
 @when(u'CANBUS上提示主驾驶安全带示系灯为{status}')
@@ -35,7 +35,6 @@ def step_impl(context, status):
         logging.error("error status:" + status)
         raise Exception
 
-
 @then(u'HUD不显示低燃油报警')
 def step_impl(context):
     context.dorothyExpRes.set_value('LowFuelWaring', 'Off')
@@ -44,3 +43,58 @@ def step_impl(context):
     logging.debug("context.dorothyExpRes.LowFuelWaring: " + str(context.dorothyExpRes.dist['LowFuelWaring']))
     logging.debug("context.dorothyActRes.LowFuelWaring: " + str(context.dorothyActRes.dist['LowFuelWaring']))
     assert context.dorothyActRes.dist['LowFuelWaring'] == context.dorothyExpRes.dist['LowFuelWaring']
+
+@given(u'HUD不显示低燃油报警')
+def step_impl(context):
+    context.dorothyExpRes.set_value('LowFuelWaring', 'Off')
+    context.dorothyActRes.mock_value('LowFuelWaring', 'Off')
+    context.dorothyActRes.get_value('LowFuelWaring')
+    logging.debug("context.dorothyExpRes.LowFuelWaring: " + str(context.dorothyExpRes.dist['LowFuelWaring']))
+    logging.debug("context.dorothyActRes.LowFuelWaring: " + str(context.dorothyActRes.dist['LowFuelWaring']))
+    assert context.dorothyActRes.dist['LowFuelWaring'] == context.dorothyExpRes.dist['LowFuelWaring']
+
+
+@when(u'CANBUS上剩余油量在{duration}秒内从{init_oil}升下降到{end_oil}升')
+def step_impl(context, duration, init_oil, end_oil):
+    context.dorothyTestInput.sysExtEvt.set_initial_value('Speed', float(init_oil))
+    context.dorothyTestInput.sysExtEvt.set_end_value('Speed', float(end_oil))
+    context.dorothyTestInput.sysExtEvt.set_signal_duration(float(duration))
+    context.dorothyTestInput.sysExtEvt.start_generate_signal()
+
+
+@then(u'HUD显示低燃油报警')
+def step_impl(context):
+    context.dorothyExpRes.set_value('LowFuelWaring', 'on')
+    context.dorothyActRes.mock_value('LowFuelWaring', 'on')
+    context.dorothyActRes.get_value('LowFuelWaring')
+    logging.debug("context.dorothyExpRes.LowFuelWaring: " + str(context.dorothyExpRes.dist['LowFuelWaring']))
+    logging.debug("context.dorothyActRes.LowFuelWaring: " + str(context.dorothyActRes.dist['LowFuelWaring']))
+    assert context.dorothyActRes.dist['LowFuelWaring'] == context.dorothyExpRes.dist['LowFuelWaring']
+
+
+@given(u'HUD显示低燃油报警')
+def step_impl(context):
+    context.dorothyExpRes.set_value('LowFuelWaring', 'on')
+    context.dorothyActRes.mock_value('LowFuelWaring', 'on')
+    context.dorothyActRes.get_value('LowFuelWaring')
+    logging.debug("context.dorothyExpRes.LowFuelWaring: " + str(context.dorothyExpRes.dist['LowFuelWaring']))
+    logging.debug("context.dorothyActRes.LowFuelWaring: " + str(context.dorothyActRes.dist['LowFuelWaring']))
+    assert context.dorothyActRes.dist['LowFuelWaring'] == context.dorothyExpRes.dist['LowFuelWaring']
+
+
+@when(u'CANBUS上剩余油量在{duration}秒内从{init_oil}升上升到{end_oil}升')
+def step_impl(context, duration, init_oil, end_oil):
+    context.dorothyTestInput.sysExtEvt.set_initial_value('Speed', int(init_oil))
+    context.dorothyTestInput.sysExtEvt.set_end_value('Speed', int(end_oil))
+    context.dorothyTestInput.sysExtEvt.set_signal_duration(int(duration))
+    context.dorothyTestInput.sysExtEvt.start_generate_signal()
+
+
+@then(u'CANBUS上剩余油量在{duration}秒内从{init_oil}升下降到{end_oil}升')
+def step_impl(context, duration, init_oil, end_oil):
+    context.dorothyExpRes.set_value('oil', int(end_oil))
+    context.dorothyActRes.mock_value('oil', int(end_oil))
+    context.dorothyActRes.get_value('oil')
+    logging.debug("context.dorothyExpRes.Speed: " + str(context.dorothyExpRes.dist['oil']))
+    logging.debug("context.dorothyActRes.Speed: " + str(context.dorothyActRes.dist['oil']))
+    assert context.dorothyActRes.dist['oil'] == context.dorothyExpRes.dist['oil']
